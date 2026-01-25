bd7f560cc51fdde9dbae84f85a292f9210035ca4
alexsylex <alex_munoz94@hotmail.com>
2026-01-25 19:24:37 +0100

    Updated dependency checkout method

--- cmake/ports/commonlibsse-ng/portfile.cmake
+++ cmake/ports/commonlibsse-ng/portfile.cmake
@@ -1,40 +1,67 @@
-# Get port from Github
+set(VCPKG_POLICY_SKIP_ABSOLUTE_PATHS_CHECK enabled)
+
+file(READ "${CMAKE_CURRENT_LIST_DIR}/version.conf" VERSION_INFO)
+
+string(REGEX MATCH "COMMONLIBSSE_REF=([a-f0-9]+)" _ "${VERSION_INFO}")
+set(COMMONLIBSSE_REF "${CMAKE_MATCH_1}")
+
+string(REGEX MATCH "COMMONLIBSSE_SHA512=([a-f0-9]+)" _ "${VERSION_INFO}")
+set(COMMONLIBSSE_SHA512 "${CMAKE_MATCH_1}")
+
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO alandtse/CommonLibVR
-    REF 2b983f5281bfadd26ee20787390d2513e8ffe38a
-    SHA512 d8bae2aa1c066def4a194c85dda57b0b7b758f3221c4d6ef7783c0e29fb7cff353085fbd297996762fd06fd191fb0f19646248e1448001f27c0cbf56cfa3a443
-    HEAD_REF ng
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO alandtse/CommonLibVR
+	REF ${COMMONLIBSSE_REF}
+	SHA512 ${COMMONLIBSSE_SHA512}
+	HEAD_REF ng
 )
 
-# Get submodule and copy to extern/ folder (done manually because Vcpkg does not support Git submodules)
+# Download OpenVR submodule
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH_OPENVR
-    REPO ValveSoftware/openvr
-    REF ebdea152f8aac77e9a6db29682b81d762159df7e
-    SHA512 4fb668d933ac5b73eb4e97eb29816176e500a4eaebe2480cd0411c95edfb713d58312036f15db50884a2ef5f4ca44859e108dec2b982af9163cefcfc02531f63
-    HEAD_REF master
+	OUT_SOURCE_PATH SOURCE_PATH_OPENVR
+	REPO ValveSoftware/openvr
+	REF ebdea152f8aac77e9a6db29682b81d762159df7e # 01/09/17
+	SHA512 4fb668d933ac5b73eb4e97eb29816176e500a4eaebe2480cd0411c95edfb713d58312036f15db50884a2ef5f4ca44859e108dec2b982af9163cefcfc02531f63
+	HEAD_REF master
 )
- file(COPY "${SOURCE_PATH_OPENVR}/" DESTINATION "${SOURCE_PATH}/extern/openvr")
- file(REMOVE_RECURSE "${SOURCE_PATH_OPENVR}/")
+file(COPY "${SOURCE_PATH_OPENVR}/" DESTINATION "${SOURCE_PATH}/extern/openvr")
+file(REMOVE_RECURSE "${SOURCE_PATH_OPENVR}/")
 
- # Configure options to build
 vcpkg_configure_cmake(
-        SOURCE_PATH "${SOURCE_PATH}"
-        PREFER_NINJA
-        OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+	OPTIONS -DBUILD_TESTS=off -DSKSE_SUPPORT_XBYAK=on
 )
-
 vcpkg_install_cmake()
+
+file(GLOB OPENVR_API_LIB "${CURRENT_BUILDTREES_DIR}/src/**/extern/openvr/lib/win64/openvr_api.lib")
+
+file(COPY "${OPENVR_API_LIB}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib/")
+
 vcpkg_cmake_config_fixup(PACKAGE_NAME CommonLibSSE CONFIG_PATH lib/cmake)
+
+file(GLOB_RECURSE TARGETS_FILES "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/*targets.cmake")
+
+foreach(targets_file ${TARGETS_FILES})
+    file(READ "${targets_file}" TARGETS_CONTENT)
+
+	string(REPLACE
+		"${OPENVR_API_LIB}"
+		"\${_IMPORT_PREFIX}/lib/openvr_api.lib"
+		TARGETS_CONTENT
+		"${TARGETS_CONTENT}"
+	)
+
+    file(WRITE "${targets_file}" "${TARGETS_CONTENT}")
+endforeach()
+
 vcpkg_copy_pdbs()
 
-file(INSTALL "${SOURCE_PATH}/extern/openvr/headers/openvr.h" DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL "${SOURCE_PATH}/extern/openvr/headers/openvr.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 file(GLOB CMAKE_CONFIGS "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE/*.cmake")
 file(INSTALL ${CMAKE_CONFIGS} DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
 file(INSTALL "${SOURCE_PATH}/cmake/CommonLibSSE.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/share/CommonLibSSE/CommonLibSSE")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
--- include/utils/Trampoline.h
+++ include/utils/Trampoline.h
@@ -2,7 +2,6 @@
 
 #if defined(SKSE_SUPPORT_XBYAK)
 
-#include <Psapi.h>
 #include <xbyak/xbyak.h>
 
 #include "SKSE/Trampoline.h"
@@ -17,8 +16,7 @@ namespace hooks
 		static constexpr std::size_t getSizeForSrc()
 		{
 			// Reference: write_5branch() and write_6branch() of Trampoline.h
-			if constexpr (SrcSize == 5)
-			{
+			if constexpr (SrcSize == 5) {
 #pragma pack(push, 1)
 				// FF /4
 				// JMP r/m64
@@ -33,9 +31,7 @@ namespace hooks
 #pragma pack(pop)
 
 				return sizeof(TrampolineAssembly);
-			}
-			else
-			{
+			} else {
 				return sizeof(std::uintptr_t);
 			}
 		}
@@ -46,15 +42,14 @@ namespace hooks
 		}
 
 	public:
-
 		Hook(std::uintptr_t a_src, const Xbyak::CodeGenerator& a_codeGen) :
 			size{ getSizeFor(a_codeGen) }, src{ a_src },
 			asmCode{ a_codeGen.getCode(), a_codeGen.getCode() + a_codeGen.getSize() }
-		{ }
+		{}
 
-		Hook(std::uintptr_t a_src, std::uintptr_t a_dst) : 
+		Hook(std::uintptr_t a_src, std::uintptr_t a_dst) :
 			size{ getSizeForSrc() }, src{ a_src }, dst{ a_dst }
-		{ }
+		{}
 
 		std::size_t getSize() const { return size; }
 
@@ -64,8 +59,7 @@ namespace hooks
 
 		void createDst(SKSE::Trampoline* a_allocator)
 		{
-			if (!asmCode.empty())
-			{
+			if (!asmCode.empty()) {
 				void* buffer = a_allocator->allocate(asmCode.size());
 				std::memcpy(buffer, asmCode.data(), asmCode.size());
 				dst = reinterpret_cast<std::uintptr_t>(buffer);
@@ -73,7 +67,6 @@ namespace hooks
 		}
 
 	private:
-
 		std::size_t size;
 		std::uintptr_t src;
 		std::uintptr_t dst = reinterpret_cast<std::uintptr_t>(nullptr);
@@ -83,7 +76,6 @@ namespace hooks
 	class Trampoline
 	{
 	public:
-
 		template <std::size_t SrcSize>
 		std::uintptr_t write_branch(Hook<SrcSize>& a_hook)
 		{
@@ -105,28 +97,29 @@ namespace hooks
 		}
 
 	protected:
-
 		Trampoline(SKSE::Trampoline& a_trampoline) :
 			inst{ &a_trampoline }
-		{ }
+		{}
 
 		Trampoline(const std::string_view& a_name) :
 			inst{ new SKSE::Trampoline{ a_name } }, deleteOnDestruct{ true }
-		{ }
+		{}
 
-		~Trampoline() { if (deleteOnDestruct) delete inst; }
+		~Trampoline()
+		{
+			if (deleteOnDestruct)
+				delete inst;
+		}
 
 		SKSE::Trampoline* inst;
 
 	private:
-
 		bool deleteOnDestruct = false;
 	};
 
 	class DefaultTrampoline : public Trampoline
 	{
 	public:
-
 		DefaultTrampoline(std::size_t a_size, bool a_trySKSEReserve = true) :
 			Trampoline{ SKSE::GetTrampoline() }
 		{
@@ -137,82 +130,26 @@ namespace hooks
 	class CustomTrampoline : public Trampoline
 	{
 	public:
-
-		// Reference: BranchTrampoline::Create() of https://github.com/ianpatt/skse64/blob/master/skse64_common/BranchTrampoline.cpp
-		CustomTrampoline(const std::string_view& a_name, void* a_module, std::size_t a_size) :
-			Trampoline{ a_name }
-		{
-			// search backwards from module base
-			auto moduleBase = reinterpret_cast<std::uintptr_t>(a_module);
-			std::uintptr_t addr = moduleBase;
-			std::uintptr_t maxDisplacement = 0x80000000 - (1024 * 1024 * 128);	// largest 32-bit displacement with 128MB scratch space
-			std::uintptr_t lowestOKAddress = (moduleBase >= maxDisplacement) ? moduleBase - maxDisplacement : 0;
-			addr--;
-
-			void* base = nullptr;
-
-			while (!base)
-			{
-				MEMORY_BASIC_INFORMATION info;
-
-				if (!VirtualQuery((void*)addr, &info, sizeof(info)))
-				{
-					break;
-				}
-
-				if (info.State == MEM_FREE)
-				{
-					// free block, big enough?
-					if (info.RegionSize >= a_size)
-					{
-						// try to allocate it
-						addr = reinterpret_cast<std::uintptr_t>(info.BaseAddress) + info.RegionSize - a_size;
-
-						base = VirtualAlloc(reinterpret_cast<void*>(addr), a_size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-					}
-				}
-
-				// move back and try again
-				if (!base)
-				{
-					addr = ((uintptr_t)info.BaseAddress) - 1;
-				}
-
-				if (addr < lowestOKAddress)
-				{
-					break;
-				}
-			}
-
-			inst->set_trampoline(base, a_size,
-				[](void* a_mem, std::size_t)
-				{
-					SKSE::WinAPI::VirtualFree(a_mem, 0, MEM_RELEASE);
-				});
-		}
+		CustomTrampoline(const std::string_view& a_name, void* a_module, std::size_t a_size);
 	};
 
 	class SigScanner
 	{
 	public:
+		static std::pair<std::uintptr_t, std::size_t> GetModuleInfo(REX::W32::HMODULE a_moduleHandle);
 
-		 template <SKSE::stl::nttp::string str>
-		static std::uintptr_t FindPattern(SKSE::WinAPI::HMODULE a_moduleHandle)
+		template <SKSE::stl::nttp::string str>
+		static std::uintptr_t FindPattern(REX::W32::HMODULE a_moduleHandle)
 		{
-			MODULEINFO moduleInfo;
-			GetModuleInformation(GetCurrentProcess(), reinterpret_cast<HMODULE>(a_moduleHandle), &moduleInfo, sizeof(MODULEINFO));
-			auto base = reinterpret_cast<std::uintptr_t>(moduleInfo.lpBaseOfDll);
-			std::size_t size = moduleInfo.SizeOfImage;
+			auto [base, size] = GetModuleInfo(a_moduleHandle);
 
-			std::size_t patternLength = (str.length() + 1) / 3; // 2/3 useful chars (1 space between byte chars)
+			std::size_t patternLength = (str.length() + 1) / 3;	 // 2/3 useful chars (1 space between byte chars)
 			auto pattern = REL::make_pattern<str>();
 
-			for (std::size_t offset = 0; offset < size - patternLength; offset++)
-			{
+			for (std::size_t offset = 0; offset < size - patternLength; offset++) {
 				std::uintptr_t addr = base + offset;
 
-				if (pattern.match(addr))
-				{
+				if (pattern.match(addr)) {
 					return addr;
 				}
 			}
